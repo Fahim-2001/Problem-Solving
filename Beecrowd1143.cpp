18d8030fe5436bd5d796d7cdc0e1d658ce8fcadc
unknown <mdyasirofficial2001@gmail.com>
2023-04-07 11:24:51 +0600

    Added Beecrowd1143

---
+++ Beecrowd1143.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int x = 1; x <= n; x++)
+    {
+        cout << x << " " << x * x << " " << x * x * x << endl;
+    }
+}
