a8f7918b0dcb39ed6038a8bc86402bea053c35d2
unknown <mdyasirofficial2001@gmail.com>
2023-04-14 15:44:25 +0600

    Added Beecrowd1158

---
+++ Beecrowd/Beecrowd1158.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tc, x, y, sum = 0, count = 0;
+    cin >> tc;
+
+    for (int i = 1; i <= tc; i++)
+    {
+        cin >> x >> y;
+        do
+        {
+            if (x % 2 != 0)
+            {
+                sum += x;
+                count++;
+            }
+            x++;
+        } while (count < y);
+        cout << sum << endl;
+    }
+}
