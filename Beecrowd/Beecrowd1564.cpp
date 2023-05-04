ea923824f639576f0a00021dd5e2448d53179f25
unknown <mdyasirofficial2001@gmail.com>
2023-05-04 20:40:15 +0600

    Added Beecrowd1564.cpp

---
+++ Beecrowd/Beecrowd1564.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tc;
+
+    while (scanf("%d", &tc) != EOF)
+    {
+        if (tc > 0)
+            cout << "vai ter duas!" << endl;
+        else
+            cout << "vai ter copa!" << endl;
+    }
+
+    return 0;
+}
