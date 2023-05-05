21b263d00afce453532763c07bcbae1bfb24356b
unknown <mdyasirofficial2001@gmail.com>
2023-05-05 23:47:57 +0600

    Added watermelonA.cpp

---
+++ Codeforce/watermelonA.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    if (n % 2 == 0 && n > 2)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+
+    return 0;
+}
