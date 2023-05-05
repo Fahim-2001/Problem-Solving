56f287191a6229794dc6ac322666ba87f776f8d8
unknown <mdyasirofficial2001@gmail.com>
2023-05-05 22:38:38 +0600

    Added Beecrowd1848.cpp

---
+++ Beecrowd/Beecrowd1848.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string blinks;
+    int sum = 0;
+
+    while (getline(cin, blinks))
+    {
+        if (blinks == "--*")
+            sum += 1;
+        else if (blinks == "*--")
+            sum += 4;
+        else if (blinks == "***")
+            sum += 7;
+        else if (blinks == "-*-")
+            sum += 2;
+        else if (blinks == "*-*")
+            sum += 5;
+        else if (blinks == "**-")
+            sum += 6;
+        else if (blinks == "-**")
+            sum += 3;
+        else if (blinks == "---")
+            sum += 0;
+        if (blinks == "caw caw")
+        {
+            cout << sum << endl;
+            sum = 0;
+        }
+    }
+}
