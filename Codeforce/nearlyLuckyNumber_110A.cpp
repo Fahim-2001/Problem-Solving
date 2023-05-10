6890c75a5f9bf6d5848bc0e824a8c2c1f6c1b5b0
unknown <mdyasirofficial2001@gmail.com>
2023-05-10 22:41:26 +0600

    Added 110A

---
+++ Codeforce/nearlyLuckyNumber_110A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long num, count = 0;
+
+    cin >> num;
+
+    while (num > 0)
+    {
+        if (num % 10 == 4 || num % 10 == 7)
+        {
+            count++;
+        }
+        num /= 10;
+    }
+
+    if (count == 4 || count == 7)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
