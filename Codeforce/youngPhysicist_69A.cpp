18a2c4c94778e767181efe5044ca7f2704a15511
unknown <mdyasirofficial2001@gmail.com>
2023-05-22 19:59:16 +0600

    Added 69A

---
+++ Codeforce/youngPhysicist_69A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tc, x, y, z, sumX = 0, sumY = 0, sumZ = 0;
+    cin >> tc;
+
+    for (int i = 1; i <= tc; i++)
+    {
+        cin >> x >> y >> z;
+        sumX += x;
+        sumY += y;
+        sumZ += z;
+    }
+
+    if (sumX == 0 && sumY == 0 && sumZ == 0)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+    return 0;
+}
