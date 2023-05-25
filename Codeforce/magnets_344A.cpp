8cfa7ff61f3f782a1cf09f0ecc4ca6fa96c7331e
unknown <mdyasirofficial2001@gmail.com>
2023-05-25 16:04:55 +0600

    Added 266B

---
+++ Codeforce/magnets_344A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, liked = 0;
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] != arr[i + 1])
+        {
+            liked++;
+        }
+    }
+    cout << liked << endl;
+    return 0;
+}
