e26d71211ddb8bfbccac692f9137ab8add246834
unknown <mdyasirofficial2001@gmail.com>
2023-06-03 21:20:21 +0600

    Added 1742A

---
+++ Codeforce/sum_1742A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tc, a, b, c;
+    cin >> tc;
+
+    int arr[3];
+
+    for (int i = 0; i < tc; i++)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            cin >> arr[i];
+        }
+        sort(arr, arr + 3, greater<int>());
+
+        if (arr[0] == arr[1] + arr[2])
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+}
