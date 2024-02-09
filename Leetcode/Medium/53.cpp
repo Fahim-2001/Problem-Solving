1fbeaf93eab5b540ebb16a0aa4860198cb327139
Fahim2001 <mdyasirofficial2001@gmail.com>
2024-02-09 23:21:39 +0600

    Leetcode 53 added (kadane's)

---
+++ Leetcode/Medium/53.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pl cout <<
+#define el << endl
+#define nl cout << endl
+#define in insert
+#define pb push_back
+#define bn begin()
+#define en end()
+#define mnel min_element
+#define mxel max_element
+#define ll long long
+#define sz size()
+#define fahim_noob 0
+#define test   \
+    int tc;    \
+    cin >> tc; \
+    while (tc--)
+
+typedef vector<int> IV;
+typedef vector<IV> VIV;
+typedef vector<string> SV;
+typedef pair<int, int> IP;
+typedef vector<IP> VIP;
+typedef map<int, int> IMP;
+typedef set<int> ISET;
+typedef set<char> ChSET;
+
+// Optimal Solution: Using Kadane's Algorithm
+int maxSubArray(vector<int> &nums)
+{
+    long long maxi = INT_MIN, sum = 0;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        sum += nums[i];
+        maxi = max(sum, maxi);
+        if (sum < 0)
+        {
+            sum = 0;
+        }
+    }
+    return maxi;
+}
+
+int main()
+{
+    IV nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    maxSubArray(nums);
+    return fahim_noob;
+}
