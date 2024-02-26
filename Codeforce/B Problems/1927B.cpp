93c3b123df6791deb029bfd3cf5dbca648c75b73
Fahim2001 <mdyasirofficial2001@gmail.com>
2024-02-26 22:41:34 +0600

    Added Codeforces 1927B

--- Codeforce/B Problems/1927B.cpp
+++ Codeforce/B Problems/1927B.cpp
@@ -1,15 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define FAST_IO (ios_base::sync_with_stdio(false), cin.tie(NULL));
+
+#define ll long long
+#define ull unsigned long long
+
 #define pl cout <<
 #define el << endl
 #define nl cout << endl
+#define ff first
+#define ss second
 #define in insert
 #define pb push_back
+#define ppb pop_back
 #define bn begin()
 #define en end()
+#define all(x) x.begin(), x.end()
 #define mnel min_element
 #define mxel max_element
-#define ll long long
 #define sz size()
 #define fahim_noob 0
 #define test   \
@@ -18,66 +26,48 @@ using namespace std;
     while (tc--)
 
 typedef vector<int> IV;
+typedef vector<ll> LIV;
 typedef vector<IV> VIV;
 typedef vector<string> SV;
 typedef pair<int, int> IP;
 typedef vector<IP> VIP;
 typedef map<int, int> IMP;
+typedef map<ll, ll> LLMP;
+typedef map<char, ll> ChLMP;
 typedef set<int> ISET;
 typedef set<char> ChSET;
-void solve(){
-    ll n, m, k;
-        cin >> n >> m >> k;
-        unordered_map<int, int> mp1, mp2;
-        for (int i = 0; i < n; i++)
-        {
-            int a;
-            cin >> a;
-            mp1[a]++;
-        }
-        for (int i = 0; i < m; i++)
-        {
-            int a;
-            cin >> a;
-            mp2[a]++;
-        }
 
-        int c1 = 0, c2 = 0;
-        int flag = 0;
-        for (int i = 1; i <= k; i++)
-        {
-            flag = 0;
-            if (mp1[i])
-            {
-                flag = 1;
-                c1++;
-            }
-            if (mp2[i])
-            {
-                flag = 1;
-                c2++;
-            }
-            if (flag == 0)
-            {
-                cout << "No" << endl;
-                return;
+void solve()
+{
+    ChLMP chmp;
+    for (char c = 'a'; c <= 'z'; c++)
+        chmp[c] = 0;
+
+    ll n;
+    cin >> n;
+    
+    string ans ="";
+    for (int i = 0; i < n; i++)
+    {
+        int q; cin>>q;
+        for(auto [x,y]: chmp){
+            if(y==q){
+                ans.push_back(x);
+                chmp[x]++;
+                break;
             }
         }
-        if (c1 >= k / 2 && c2 >= k / 2)
-        {
-            cout << "Yes" << endl;
-        }
-        else
-        {
-            cout << "No" << endl;
-        }
+    }
+
+    pl ans el;
 }
+
 int main()
 {
+    FAST_IO;
     test
     {
         solve();
     }
-
     return fahim_noob;
 }
