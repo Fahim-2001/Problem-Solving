27a39c0dec802da6fde66ce0d857d97aa4422220
Fahim2001 <mdyasirofficial2001@gmail.com>
2024-03-22 20:38:26 +0600

    Added Codeforces 1611B

---
+++ Codeforce/B Problems/1611B.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FAST_IO (ios_base::sync_with_stdio(false), cin.tie(NULL));
+
+#define ll long long
+#define ull unsigned long long
+
+#define pl cout <<
+#define el << endl
+#define nl cout << endl
+#define ff first
+#define ss second
+#define in insert
+#define pb push_back
+#define ppb pop_back
+#define bn begin()
+#define en end()
+#define all(x) x.begin(), x.end()
+#define mnel min_element
+#define mxel max_element
+#define sz size()
+#define fahim_noob 0
+#define test   \
+    int tc;    \
+    cin >> tc; \
+    while (tc--)
+
+typedef vector<int> IV;
+typedef vector<ll> LIV;
+typedef vector<IV> VIV;
+typedef vector<string> SV;
+typedef pair<int, int> IP;
+typedef vector<IP> VIP;
+typedef map<int, int> IMP;
+typedef map<ll, ll> LLMP;
+typedef map<char, ll> ChLMP;
+typedef set<int> ISET;
+typedef set<char> ChSET;
+
+void solve()
+{
+    ll a, b;
+    cin >> a >> b;
+    pl min(min(a,b), (a+b)/4) el;    
+}
+
+int main()
+{
+    FAST_IO;
+    test
+    {
+        solve();
+    }
+    return fahim_noob;
+}
