031d3fc8a15d40042a419f703fab942d4e52d42a
Fahim-2001 <mdyasirofficial2001@gmail.com>
2024-04-20 22:50:01 +0600

    1575A Codeforces added

---
+++ Codeforce/A Problems/1575A.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+using namespace chrono;
+#define FAST_IO (ios_base::sync_with_stdio(false), cin.tie(NULL));
+
+#define ll long long
+#define ull unsigned long long
+
+#define pl cout <<
+#define el << endl
+#define nl cout << endl
+#define ff first
+#define ss second
+#define in insert
+#define pb push_back
+#define ppb pop_back
+#define bn begin()
+#define en end()
+#define all(x) x.begin(), x.end()
+#define mnel min_element
+#define mxel max_element
+#define sz size()
+#define fahim_noob 0
+#define test   \
+    int tc;    \
+    cin >> tc; \
+    while (tc--)
+
+#define arrIn(start, n)         \
+    for (int i = 0; i < n; i++) \
+        cin >> arr[i];
+typedef vector<int> IV;
+typedef vector<ll> LIV;
+typedef vector<IV> VIV;
+typedef vector<string> SV;
+typedef pair<int, int> IP;
+typedef vector<IP> VIP;
+typedef map<int, int> IMP;
+typedef map<ll, ll> LLMP;
+typedef map<char, ll> ChLMP;
+typedef set<int> ISET;
+typedef set<char> ChSET;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    int x = n % 1000;
+    int y = n / 1000;
+
+    int sum1 = 0, sum2 = 0;
+    while (x > 0)
+    {
+        sum1 += x % 10;
+        x /= 10;
+    }
+
+    while (y > 0)
+    {
+        sum2 += y % 10;
+        y /= 10;
+    }
+
+    if (sum1 == sum2)
+        pl "Yes" el;
+    else
+        pl "No" el;
+}
+
+int main()
+{
+    FAST_IO;
+    auto start = high_resolution_clock::now();
+    test
+    {
+        solve();
+    }
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+    cerr << "Time taken: " << duration.count() << " ms" << endl;
+    return fahim_noob;
+}
